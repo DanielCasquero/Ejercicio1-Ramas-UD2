3c8a507a3f72d8877ce7faec9557b5cd63cf2b44
Daniel Casquero <24.daw.danielcasquero@colegiomontessori.com>
2024-11-22 17:41:19 +0100

    CommitSaludosNombre

--- Proyecto/src/saludos/Saludos.java
+++ Proyecto/src/saludos/Saludos.java
@@ -6,12 +6,13 @@ public class Saludos {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		
 		System.out.print("Nombre: ");
 		String nombre = input.nextLine();
 		System.out.print("Apellido: ");
 		String apellido = input.nextLine();
 		System.out.println("Buenos tardes " + nombre + " " + apellido);
-
+		
 		input.close();
 	}
 
