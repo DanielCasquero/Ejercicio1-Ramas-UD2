e9837d7360c61ba3a00008054aa34669bec6b7d9
Daniel Casquero <24.daw.danielcasquero@colegiomontessori.com>
2024-11-22 17:19:42 +0100

    SaludosHora(EnRamaSaludosNombreXDPerdon)

--- Proyecto/src/saludos/Saludos.java
+++ Proyecto/src/saludos/Saludos.java
@@ -1,9 +1,26 @@
 package saludos;
 
-public class Saludos {
+import java.util.Scanner;
 
+public class Saludos {
+	
+	static final int HORA = 12;
+	
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
+		Scanner input = new Scanner(System.in);
+		System.out.print("Nombre: ");
+		String nombre = input.nextLine();
+		System.out.print("Apellido: ");
+		String apellido = input.nextLine();
+		System.out.println("Introduzca la hora (formato 24h), sin introducir los minutos");
+		int horaActual =input.nextInt();
+		
+		if (horaActual <= HORA) {
+			System.out.println("Buenos dias " + nombre + " " + apellido);
+		}else if (horaActual > HORA) {
+			System.out.println("Buenos tardes " + nombre + " " + apellido);
+		}
+		input.close();
 	}
 
 }
